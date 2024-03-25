20a3e78bb84284b549836e4af73f6ffdd3ee40ab
Emirhan Yagci <emirhan.yac2@gmail.com>
2024-03-25 12:55:06 +0300

    project assets added

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,17 @@
+import { Routes, Route } from "react-router-dom";
+import Home from "./tabs/Home.jsx";
+import MainLayout from "./components/MainLayout.jsx";
 function App() {
-  return <div className="">Hello sdweather app</div>;
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<MainLayout />}>
+          <Route index path="/" element={<Home />}></Route>
+          <Route path="/weather"></Route>
+        </Route>
+      </Routes>
+    </>
+  );
 }
 
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,49 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       backgroundImage: {
-        mainLayout: "url('/src/assets/images/mainLayout.png')",
-        todayResultDark: "url('/src/assets/images/todayResultDark.png')",
-        todayResultLight: "url('/src/assets/images/todayResultLight.png')",
+        mainLayout: "url('/images/mainLayout.png')",
+        todayResultDark: "url('/images/todayResultDark.png')",
+        todayResultLight: "url('v/images/todayResultLight.png')",
+      },
+      colors: {
+        baseBlueLight: "#8FB2F5",
+        base: {
+          white: "#FFFFFF",
+          100: "#FAFAFA",
+          200: "#BFBFD4",
+          300: "#ABABC4",
+          400: "#7F7F98",
+          500: "#3B3B54",
+          600: "#22222F",
+          700: "#1C1C27",
+          800: "#16161F",
+          900: "#13131A",
+        },
+      },
+      fontFamily: {
+        nunito: ["nunito", ...defaultTheme.fontFamily.sans],
       },
     },
   },
 };
+// /** @type {import('tailwindcss').Config} */
+// export default {
+//   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+//   theme: {
+//     extend: {
+//       backgroundImage: {
+//         mainLayout: "url('/src/assets/images/mainLayout.png')",
+//         todayResultDark: "url('/src/assets/images/todayResultDark.png')",
+//         todayResultLight: "url('/src/assets/images/todayResultLight.png')",
+//       },
+//       textColor: {
+//         primaryLight: ({ theme }) => theme('colors.gray.100'), // Use any color from Tailwind CSS color palette
+//       },
+//     },
+//   },
+// };
