f2f131ee9b51d60bb854858032b6eb87ff224c35
Emirhan Yagci <emirhan.yac2@gmail.com>
2024-03-28 20:46:22 +0300

    weatherDetails component completed

--- src/components/WeatherDetailItem.jsx
+++ src/components/WeatherDetailItem.jsx
@@ -1,11 +1,11 @@
-export default function WeatherDetailItem() {
+export default function WeatherDetailItem({ label, value, icon }) {
   return (
     <li className="flex items-center justify-between border-b-[1px] border-b-base-700 py-4 last:border-none">
       <div className="flex items-center space-x-3">
-        <img src="/icons/thermometer.svg" alt="" />
-        <span className="heading-xs text-base-200">Thermal sensation</span>
+        <img src={`/icons/${icon}`} alt="" />
+        <span className="heading-xs text-base-200">{label}</span>
       </div>
-      <div className="heading-sm">26ºc</div>
+      <div className="heading-sm">{value}</div>
     </li>
   );
 }
--- src/components/WeatherDetails.jsx
+++ src/components/WeatherDetails.jsx
@@ -1,13 +1,36 @@
+import { useWeatherContext } from "../context/WeatherContext";
 import CardContainer from "./CardContainer";
 import WeatherDetailItem from "./WeatherDetailItem";
 
 export default function WeatherDetails() {
+  const { weatherData } = useWeatherContext();
+  const { feels_like, clouds, wind_speed, humidity, uvi } =
+    weatherData.currentWeather.data[0];
+
   return (
     <CardContainer py={false}>
       <ul>
-        {Array.from({ length: 5 }, (v, i) => i).map((item, index) => (
-          <WeatherDetailItem key={index} />
-        ))}
+        <WeatherDetailItem
+          label="Thermal sensation"
+          value={Math.round(feels_like) + "ºc"}
+          icon="thermometer.svg"
+        />
+        <WeatherDetailItem
+          label="Probability of rain"
+          value={Math.round(clouds) + "%"}
+          icon="cloud-rain.svg"
+        />
+        <WeatherDetailItem
+          label="Wind speed"
+          value={Math.round(wind_speed) + " m/s"}
+          icon="wind.svg"
+        />
+        <WeatherDetailItem
+          label="Air humidity"
+          value={Math.round(humidity) + "%"}
+          icon="drop.svg"
+        />{" "}
+        <WeatherDetailItem label="UV Index" value={uvi} icon="sun.svg" />
       </ul>
     </CardContainer>
   );
